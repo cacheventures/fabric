11f80410c71630e379b3a8c9457e483da4358eeb
evan berquist <e@cache.vc>
2018-04-23 18:28:22 -0400

    update subscription items when subscription is updated

--- lib/fabric/update_subscription_operation.rb
+++ lib/fabric/update_subscription_operation.rb
@@ -16,10 +16,17 @@ module Fabric
         @subscription.stripe_id
       )
 
-      @attributes.each { |k,v| stripe_subscription.send("#{k}=", v) }
+      @attributes.each { |k, v| stripe_subscription.send("#{k}=", v) }
       stripe_subscription.save
 
       @subscription.sync_with(stripe_subscription)
+      stripe_subscription.items.data.each do |sub_item|
+        item = @subscription.subscription_items.find_by(
+          stripe_id: sub_item.id
+        )
+        item.sync_with(sub_item)
+        item.save
+      end
       saved = @subscription.save
       Fabric.config.logger.info "UpdateSubscriptionOperation: Completed. "\
         "saved: #{saved}"

019296cec13b9268b9f2657d538a4a121e80c112
Daniel Arnold <darnold8@gmail.com>
2018-04-23 18:29:47 -0400

    fix SyncPlansOperation to work with any number of plans

--- lib/fabric/sync_plans_operation.rb
+++ lib/fabric/sync_plans_operation.rb
@@ -5,7 +5,16 @@ module Fabric
     def call
       Fabric.config.logger.info "SyncPlansOperation: Fetching all plans and "\
         "syncing with any local copies."
-      stripe_plans = Stripe::Plan.all
+      plan_list = Stripe::Plan.list(limit: 100)
+
+      # iterate over list and continue pulling all plans
+      stripe_plans = []
+      stripe_plans.push(*plan_list)
+      while plan_list.has_more?
+        plan_list = plan_list.next_page
+        stripe_plans.push(*plan_list)
+      end
+
       stripe_plans.each do |p|
         plan = Fabric::Plan.find_by(stripe_id: p.id) || Fabric::Plan.new
         plan.sync_with(p)

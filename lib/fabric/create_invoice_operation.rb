de5aca6ddd0a66b54ca3dd18e594922d1f8ed3ab
Jarrett Lusso <jclusso@gmail.com>
2019-08-30 09:54:17 -0400

    Made it so customer isn't needed in invoice related operations to more closely mirror stripes api.

--- lib/fabric/create_invoice_operation.rb
+++ lib/fabric/create_invoice_operation.rb
@@ -2,20 +2,22 @@ module Fabric
   class CreateInvoiceOperation
     include Fabric
 
-    def initialize(customer, attributes = {})
-      Fabric.config.logger.info "CreateInvoiceOperation: Started with "\
-        "#{customer} #{attributes}"
-      @customer = get_document(Fabric::Customer, customer)
+    def initialize(attributes = {})
+      @log_data = { class: self.class.name, attributes: attributes }
+      flogger.json_info 'Started', @log_data
+
       @attributes = attributes
     end
 
     def call
       stripe_invoice = Stripe::Invoice.create(@attributes)
-      invoice = @customer.invoices.build
+
+      invoice = Invoice.new
       invoice.sync_with stripe_invoice
       saved = invoice.save
-      Fabric.config.logger.info "CreateInvoiceOperation: Completed. "\
-        "saved: #{saved}"
+
+      flogger.json_info 'Completed', @log_data.merge(saved: saved)
+
       [invoice, stripe_invoice]
     end
   end

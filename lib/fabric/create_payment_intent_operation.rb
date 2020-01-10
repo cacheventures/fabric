34160328c21d88e426367bb67b8e8e4cdfc88383
Daniel Arnold <darnold8@gmail.com>
2020-01-10 13:36:16 -0500

    add create_payment_intent_operation

---
+++ lib/fabric/create_payment_intent_operation.rb
@@ -0,0 +1,24 @@
+module Fabric
+  class CreatePaymentIntentOperation
+    include Fabric
+
+    def initialize(customer, attributes = {})
+      @log_data = {
+        class: self.class.name, customer: customer, attributes: attributes
+      }
+      Fabric.config.logger.json_info 'started', @log_data
+      @customer = get_document(Fabric::Customer, customer) if customer
+      @attributes = attributes
+    end
+
+    def call
+      stripe_pi = Stripe::PaymentIntent.create(@attributes)
+      pi = @customer ? @customer.payment_intents.build : PaymentIntent.new
+      pi.sync_with(stripe_pi)
+      saved = pi.save
+      Fabric.config.logger.info 'completed', saved: saved
+      pi
+    end
+
+  end
+end

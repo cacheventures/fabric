da0896d5e8e3ca7d110d72a935df0366ba21a1ae
Daniel Arnold <darnold8@gmail.com>
2020-01-24 16:57:25 -0500

    add update_payment_intent_operation & update version

---
+++ lib/fabric/update_payment_intent_operation.rb
@@ -0,0 +1,27 @@
+module Fabric
+  class UpdatePaymentIntentOperation
+    include Fabric
+
+    def initialize(payment_intent, attributes = {})
+      @log_data = {
+        class: self.class.name, customer: customer, attributes: attributes
+      }
+      Fabric.config.logger.json_info 'started', @log_data
+
+      @payment_intent = get_document(Fabric::PaymentIntent, payment_intent)
+      @attributes = attributes
+    end
+
+    def call
+      stripe_pi = Stripe::PaymentIntent.update(
+        @payment_intent.stripe_id,
+        attributes
+      )
+      @payment_intent.sync_with(stripe_pi)
+      saved = @payment_intent.save
+      Fabric.config.logger.json_info 'completed', saved: saved
+      @payment_intent
+    end
+
+  end
+end

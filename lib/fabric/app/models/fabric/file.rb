03bd85853ced66516862a6e77f4c415a0edd2d91
evan berquist <e@cache.vc>
2020-03-06 08:53:41 -0500

    remove old dispute created webhook. add file support

---
+++ lib/fabric/app/models/fabric/file.rb
@@ -0,0 +1,28 @@
+module Fabric
+  class File
+    include Mongoid::Document
+    include Mongoid::Timestamps
+
+    field :stripe_id, type: String
+    field :purpose, type: String
+    field :type, type: String
+    field :object, type: String
+    field :created, type: Time
+    field :filename, type: String
+    field :links, type: Hash
+    field :size, type: Integer
+    field :url, type: String
+
+    def sync_with(file)
+      self.stripe_id = file.id
+      self.purpose = file.purpose
+      self.type = file.type
+      self.object = file.object
+      self.created = file.created
+      self.filename = file.filename
+      self.links = file.links.try(:to_hash)
+      self.size = file.size
+      self.url = file.url
+    end
+  end
+end

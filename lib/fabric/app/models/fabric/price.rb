83bb32f1a73ea8164a8d9d70d42f89fe3e32944a
Daniel Arnold <darnold8@gmail.com>
2020-06-08 17:40:35 -0400

    add products and prices

---
+++ lib/fabric/app/models/fabric/price.rb
@@ -0,0 +1,54 @@
+module Fabric
+  class Price
+    include Mongoid::Document
+    include Mongoid::Timestamps
+    extend Enumerize
+
+    belongs_to :product, class_name: 'Fabric::Product'
+
+    field :stripe_id, type: String
+    field :active, type: Boolean
+    field :billing_scheme, type: String
+    field :created, type: Time
+    field :currency, type: String
+    field :livemode, type: Boolean
+    field :lookup_key, type: String
+    field :metadata, type: Hash
+    field :nickname, type: String
+    field :recurring, type: Hash
+    # field :tiers, type: Array # excluded; not expanded by default
+    field :tiers_mode, type: String
+    field :transform_quantity, type: Hash
+    field :type, type: String
+    field :unit_amount, type: Integer
+    field :unit_amount_decimal, type: String # decimal string
+
+    validates_uniqueness_of :stripe_id
+    validates :stripe_id, :currency, presence: true
+
+    index({ stripe_id: 1 }, { background: true, unique: true })
+
+    def sync_with(price)
+      self.stripe_id = Fabric.stripe_id_for price
+      self.active = price.active
+      self.billing_scheme = price.billing_scheme
+      self.created = price.created
+      self.currency = price.currency
+      self.livemode = price.livemode
+      self.lookup_key = price.lookup_key
+      self.metadata = Fabric.convert_metadata(price.metadata.to_hash)
+      self.nickname = price.nickname
+      self.product = Fabric::Product.find_by(
+        stripe_id: price.product
+      ) unless product.present?
+      self.recurring = price.recurring.try(:to_hash)
+      self.tiers_mode = price.tiers_mode
+      self.transform_quantity = price.transform_quantity.try(:to_hash)
+      self.type = price.type
+      self.unit_amount = price.unit_amount
+      self.unit_amount_decimal = price.unit_amount_decimal
+      self
+    end
+
+  end
+end
---
+++ lib/fabric/sync_products_and_prices_operation.rb
@@ -0,0 +1,25 @@
+module Fabric
+  class SyncProductsAndPricesOperation
+    include Fabric
+
+    def call
+      Fabric.config.logger.json_info 'syncing products and prices',
+        class: self.class.to_s
+
+      Stripe::Product.list.auto_paging_each do |p|
+        product = Fabric::Product.find_by(
+          stripe_id: p.id
+        ) || Fabric::Product.new
+        product.sync_with(p)
+        product.save
+      end
+
+      Stripe::Price.list.auto_paging_each do |p|
+        price = Fabric::Price.find_by(stripe_id: p.id) || Fabric::Price.new
+        price.sync_with(p)
+        price.save
+      end
+    end
+
+  end
+end

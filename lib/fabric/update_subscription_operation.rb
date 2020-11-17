518d8bc7d9e8b39b1c9c5be010a122cd3bbf65a2
Jarrett Lusso <jclusso@gmail.com>
2020-11-17 06:47:40 -0500

    Updated subscription operation to return objects

--- lib/fabric/update_subscription_operation.rb
+++ lib/fabric/update_subscription_operation.rb
@@ -23,6 +23,8 @@ module Fabric
 
       Fabric.config.logger.info "UpdateSubscriptionOperation: Completed. "\
         "saved: #{saved}"
+
+      [@subscription.reload, stripe_subscription]
     end
   end
 end

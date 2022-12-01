8024bc9181010e98c5b427544a8320b1dcae9398
Jarrett Lusso <jclusso@gmail.com>
2022-12-01 09:21:41 -0500

    Remove hardcoded id_types so we don't have to update the gem when Stripe adds new types.

--- lib/fabric/app/models/fabric/tax_id.rb
+++ lib/fabric/app/models/fabric/tax_id.rb
@@ -2,7 +2,6 @@ module Fabric
   class TaxId
     include Mongoid::Document
     include Mongoid::Timestamps
-    extend Enumerize
 
     belongs_to :customer, class_name: 'Fabric::Customer',
       primary_key: :stripe_id
@@ -10,13 +9,6 @@ module Fabric
     field :stripe_id, type: String
     field :country, type: String
     field :id_type, type: String
-    enumerize :id_type, in: %w(
-      ae_trn au_abn au_arn br_cnpj br_cpf ca_bn ca_gst_hst ca_pst_bc ca_pst_mb
-      ca_pst_sk ca_qst ch_vat cl_tin es_cif eu_vat gb_vat ge_vat hk_br id_npwp
-      il_vat in_gst jp_cn jp_rn kr_brn li_uid mx_rfc my_frp my_itn my_sst no_vat
-      nz_gst ru_inn ru_kpp sa_vat sg_gst sg_uen th_vat tw_vat ua_vat us_ein
-      za_vat
-    )
     field :value, type: String
     field :created, type: Time
     field :verification, type: Hash
